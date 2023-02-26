4cff858d02099158f8bd8917f3f34beed0cbd221
León Serpa <leon.serpa@correo.unimet.edu.ve>
2023-02-26 01:35:47 -0400

    avance1

--- MetroMed/src/main.jsx
+++ MetroMed/src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Route, Routes, BrowserRouter } from "react-router-dom"
-import HomePage from '/src/Pages/HomePage/HomePage.jsx'
-import LoginPage from '/src/Pages/LoginPage/LoginPage.jsx'
-import RegisterPage from '/src/Pages/RegisterPage/RegisterPage.jsx'
-import Plantilla from './Components/Plantilla/Plantilla'
-import CatalogoPage from '/src/Pages/RegisterPage/CatalogoPage.jsx'
+import HomePage from '/src/pages/HomePage/HomePage.jsx'
+import LoginPage from '/src/pages/LoginPage/LoginPage.jsx'
+import RegisterPage from '/src/pages/RegisterPage/RegisterPage.jsx'
+import CatalogoPage from '/src/pages/CatalogoPage/CatalogoPage.jsx'
 import { HOME_URL, LOGIN_URL, SIGNIN_URL, CATALOGO_URL } from './constants/urls'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,8 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path={HOME_URL} element={<HomePage />} />
         <Route path={CATALOGO_URL} element={<CatalogoPage/>}/>
-        <Route path={SIGNIN_URL} element={<LoginPage/>}/>
-        <Route path={LOGIN_URL} element={<RegisterPage />} />
+        <Route path={SIGNIN_URL} element={<RegisterPage/>}/>
+        <Route path={LOGIN_URL} element={<LoginPage />} />
         <Route path="*" element={<h1>ERROR 404!!!!!</h1>} />
       </Routes>
     </BrowserRouter>
