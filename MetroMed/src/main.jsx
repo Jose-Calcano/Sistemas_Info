b5c4b32fe3de55b54910d584ce8615a69e77a7c5
León Serpa <leon.serpa@correo.unimet.edu.ve>
2023-02-26 00:51:50 -0400

    inicio2

--- MetroMed/src/main.jsx
+++ MetroMed/src/main.jsx
@@ -14,14 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<Plantilla />}>
-          <Route path={HOME_URL} element={<HomePage />} />
-          <Route path={CATALOGO_URL} element={<CatalogoPage/>}/>
-          <Route path={SIGNIN_URL} element={<LoginPage/>}/>
-          <Route path={LOGIN_URL} element={<RegisterPage />} />
-          <Route path="*" element={<h1>ERROR 404!!!!!</h1>} />
-        </Route>
-
+        <Route path={HOME_URL} element={<HomePage />} />
+        <Route path={CATALOGO_URL} element={<CatalogoPage/>}/>
+        <Route path={SIGNIN_URL} element={<LoginPage/>}/>
+        <Route path={LOGIN_URL} element={<RegisterPage />} />
+        <Route path="*" element={<h1>ERROR 404!!!!!</h1>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
