2cee0c841689becb5ea2b0d7048eedf80b78d1b6
Jose Calcaño <jose.calcano.dev@gmail.com>
2023-02-28 09:07:13 -0500

    Firebase Conectado
    Registro e inicio de Sesión

--- Chat/Search.jsx
+++ Chat/Search.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <div className="userChatInfo">
+          <span>Roberto</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
