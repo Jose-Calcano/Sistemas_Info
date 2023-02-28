26919ae395a1c803f152a06518bb9cf555dd0f37
Jose Calcaño <jose.calcano.dev@gmail.com>
2023-02-28 04:18:31 -0500

    Add files via upload

---
+++ Chat/Chat_navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Chat_navbar = () => {
+  return (
+    <div className="chat_navbar">
+    <span className="logo">Metromed chat</span>
+    <div className="user">
+      <img src='' alt=''/>
+      <span> Ayuda</span>
+      <button>logout</button>
+    </div>
+  </div>
+  )
+}
+
+export default Chat_navbar
