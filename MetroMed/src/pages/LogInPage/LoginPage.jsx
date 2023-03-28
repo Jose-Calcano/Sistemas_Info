227df8daf7fabadfe58ac29b3255fb40b67d71d5
Jose Calcaño <jose.calcano.dev@gmail.com>
2023-03-28 08:02:31 -0400

    Delete MetroMed directory

--- MetroMed/src/pages/LogInPage/LoginPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './LoginPage.module.css'
-
-export default function LoginPage() {
-    return (
-        <div>Iniciar Sesiòn</div>
-    )
-}
--- MetroMed/src/pages/RegisterPage/RegisterPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './RegisterPage.module.css'
-
-export default function RegisterPage() {
-    return (
-        <div>RegisterPage</div>
-    )
-}
